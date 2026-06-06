a143e8e8f86d165660061bc8e45b8fcfd00fe6c1
UCP India <ucpindia.official@gmail.com>
2026-06-07 00:16:41 +0200

    Site update 2026-06-07 00:16:41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ucp-v21';
+const CACHE = 'ucp-v22';
 const CORE = ['/', '/index.html', '/style.css', '/assets/logo.jpg', '/assets/icon-192.png', '/manifest.webmanifest', '/about.html', '/manifesto.html', '/join.html'];
 self.addEventListener('install', function(e){ e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(CORE); }).then(function(){ return self.skipWaiting(); })); });
 self.addEventListener('activate', function(e){ e.waitUntil(caches.keys().then(function(ks){ return Promise.all(ks.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); })); }).then(function(){ return self.clients.claim(); })); });
